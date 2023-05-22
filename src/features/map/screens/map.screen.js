5336fbbb1ff7a3417196be41bb800ed07f751a7f
Connie Skomra <connie@cskomra.com>
2023-05-22 15:40:48 -0400

    add map search location

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
 import { Search } from "../components/search.component";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantContext } from "../../../services/restaurants/restaurants.context";
 
 const Map = styled(MapView)`
     height: 100%;
@@ -9,9 +11,30 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = () => {
+    const { location } = useContext(LocationContext);
+    const { restaurants = [] } = useContext(RestaurantContext);
+
+    const [latDelta, setLatDelta] = useState();
+    const { lat, lng, viewport } = location;
+
+    useEffect(() => {
+        const northeastLat = viewport.northeast.lat;
+        const southwestLat = viewport.southwest.lat;
+        setLatDelta(northeastLat - southwestLat);
+    }, [location, viewport])
+
     return (
         <>
             <Search />
-            <Map />
+            <Map
+                region={{
+                    latitude: lat,
+                    longitude: lng,
+                    latitudeDelta: latDelta,
+                    longitudeDelta: 0.02,
+                }}
+            >
+                {restaurants.map((restaurant) => null)}
+            </Map>
         </>
 )};
