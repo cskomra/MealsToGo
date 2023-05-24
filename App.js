815c4b240a4ea5713668b7bca81608f62164ead9
Connie Skomra <connie@cskomra.com>
2023-05-24 11:18:58 -0400

    add authentication

--- App.js
+++ App.js
@@ -11,18 +11,6 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
 import { Navigation } from "./src/infrastructure/navigation";
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBP8zas5dC4mChIKy1KBaqDwbQowXStKM4",
-  authDomain: "mealstogo-83c56.firebaseapp.com",
-  projectId: "mealstogo-83c56",
-  storageBucket: "mealstogo-83c56.appspot.com",
-  messagingSenderId: "110462527600",
-  appId: "1:110462527600:web:460a9b86cf01e0252b6cb5"
-};
-
-initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
---
+++ src/services/authentication/authentication.context.js
@@ -0,0 +1,37 @@
+import React, { useState, createContext } from "react";
+import { loginRequest } from "./authentication.service";
+
+export const AuthenticationContext = createContext();
+
+export const AuthenticationContextProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+
+    const onLogin = (email, password) => {
+        setIsLoading(true);
+        loginRequest(email, password)
+        .then((userCredential) => {
+            setUser(userCredential.user);
+            setIsAuthenticated(true);
+            setIsLoading(false);
+          }) 
+          .catch((err) => { 
+            const errorCode = err.code;
+            const errorMessage = err.message;
+            console.log("Auth Error code: ", errorCode, "Error message: ", errorMessage)})
+    };
+
+    return (
+        <AuthenticationContext.Provider
+            value={{
+                user,
+                isLoading,
+                error,
+                onLogin,
+            }}
+        >
+            {children}
+        </AuthenticationContext.Provider>
+    )
+}
